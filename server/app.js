f5f0d215734b180582530cfcc8b9961d2dc414c8
Peiying Wen <wenpeiying@gmail.com>
2014-12-03 15:33:34 -0800

    re-arrange routes so they're more express-y

--- server/app.js
+++ server/app.js
@@ -5,15 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+var router = require('./router')(app);
 
 if (app.get('env') === 'development') {
   app.use(express.static(path.join(__dirname, '../client')));
