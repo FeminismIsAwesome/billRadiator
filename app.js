395ead9b20b442fb9b303e8fcbf6ae09f1f5047a
Ian Norris <ian.norris.1991@gmail.com>
2014-12-03 08:31:31 -0800

    Updated to remove unnecessary boiler

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.user(express.static(path.join(__dirname, 'public')));
-app.use('/',routes);
